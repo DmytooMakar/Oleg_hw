c86870749631c80f56553311153faf148c528402
Dmytro Makar <mishamakar5@gmail.com>
2022-12-16 12:40:37 +0200

    pre-last commit

--- app.js
+++ app.js
@@ -65,8 +65,8 @@ document.querySelector('#task2').onclick = function(){
 //task 3
 
 document.querySelector('#task3').onclick = function (){
-    const usersHaveCar = arr.filter(el => el.car ==='BMW' && el.haveBusiness === false)
-    console.log(usersHaveCar);
+    const usersHaveCar = arr.filter(el => el.car ==='BMW' && !el.haveBusiness)
+    console.log(usersHaveCar+ ' ВИПРАВИВ');
 }
 
 //task 4
@@ -96,34 +96,44 @@ document.querySelector('#task6').onclick = function(){
 //task 7
 
 document.querySelector('#task7').onclick = function(){
-    const sortForAge = arr.reduce((acm, el) => acm += el, '');
-    console.log(sortForAge)
+    const sortForAge = arr.reduce((acc, user) => acc<user.age?acc = user.age:acc , 0);
+    console.log(sortForAge+ ' Вгадав')
 }
 
 //task 8 
 
 document.querySelector('#task8').onclick = function(){
-    const countCar = arr.reduce((acm, user) => acm += user.car === 'BMW', 0)
+    const countCar = arr.reduce((acc, user) => acc += user.car === 'BMW', 0)
     console.log('Users have BMW '+ countCar)
 }
 
 //task 9
 
 document.querySelector('#task9').onclick = function(){
-    const countCar = arr.reduce((acm, user) =>acm += user.car === 'BMW', 0)
-    console.log({BMW: countCar})
+    console.log('ПІДГЛЯНУВ');
+    const countCar = arr.reduce((acm, user) => { 
+        if(!acm[user.car]){
+            acm[user.car] = 1;
+        } else {
+            acm[user.car] = acm[user.car]+ 1;
+        }   
+        return acm;
+    } , {})
+    console.log(countCar)
 }
 
 //task 10 
 
 document.querySelector('#task10').onclick = function(){
     const arrOfNumbers = [4, 2, 4, 7, 8, 4, 6, 2, 1, 3, 4, 7, 4, 5, 9, 3, 5, 1];
-    const array =[];
-    for(let i=1;i<10;i++){
-        const resultFilter = arrOfNumbers.filter(num => num===i);
-        array.push({i: resultFilter.length})
-    }
-    console.log(array);
+    const resultArr = arrOfNumbers.reduce((acc, num)=> {if(!acc[num]){
+        acc[num] = 1;
+    } else {
+        acc[num] = acc[num]+ 1;
+    }   
+    return acc;}
+    , {})
+    console.log(resultArr+ ' ПІДГЛЯНУВ')
 }
 
 //task 11
@@ -141,8 +151,26 @@ document.querySelector('#task11').onclick = function(){
 //task 12
 
 document.querySelector('#task12').onclick = function(){
-    console.log('Не дуже зрозумів завдання')
-    const date = new Date
-    let time = date.toJSON().split('T')[1].split('.')[0];
-    console.log('Time now is '+ time)
+    let hour = 23;
+        let minutes = 59;
+        let seconds = 55;
+        console.log('криво працює')
+    setInterval(() => {
+        seconds += 1
+        if (seconds === 0) {
+            seconds += 1
+        }   else if(seconds === 60){
+            seconds = 00;
+            minutes += 1
+        }   else if (minutes===59&&seconds === 59){
+            minutes = 00;
+            seconds = 00;
+            hour += 1
+        }   else if (hour === 24){
+            hour =00;
+            minutes = 00;
+            seconds = 00
+        }
+        console.log(hour+':'+minutes+':'+seconds)
+    }, 1000);
 }
